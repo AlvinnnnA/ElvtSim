f0fbd3437e698b753c3101c9b6a38584e2f937bf
Sihan <alvinalan01@outlook.com>
2023-03-17 22:06:15 +0800

    config

--- Scheduler/Logic/my_parser.py
+++ Scheduler/Logic/my_parser.py
@@ -1,5 +1,6 @@
 from Scheduler.Thread.thread import Elevator
 import json
+from itertools import combinations
 
 '''
 elevator_one_choice = [3, 5]
@@ -21,10 +22,41 @@ def elevator_parser(elevator_json):  # 传入一个电梯配置文件的json
 '''
 
 def elevator_parser(elevator_json):  # 传入一个电梯配置文件的json
+    # TODO json config parsing
     elevator_parse_dict = json.loads(elevator_json)  # 生成字典文件
     #for elevator in elevator_parse_dict[]:
-        
+
+
+def allocate_floors(floors, elevator_count):
+    # TODO reduce the allocation to only sensible ones
+    # generate all possible floor combinations for a single elevator
+    elevator_floors = []
+    for i in range(1, len(floors) + 1):
+        for c in combinations(floors, i):
+            elevator_floors.append(frozenset(c))
+
+    # generate all possible combinations of elevator configurations
+    elevator_configs = set()
+    for c in combinations(elevator_floors, elevator_count):
+        # check if all floors are accessible in this configuration
+        all_floors = set(floors)
+        for floors in c:
+            all_floors -= floors
+        if not all_floors:
+            # add this configuration to the result
+            config = tuple(sorted(c))
+            elevator_configs.add(config)
+
+    # convert the result to a list of dictionaries
+    result = []
+    for config in elevator_configs:
+        result.append({i + 1: sorted(floors) for i, floors in enumerate(config)})
+
+    return result
 
 def passenger_parser():
     pass
 
+
+if __name__=="__main__":
+    print(len(allocate_floors(range(1,7), 2)))
--- Scheduler/scheduler.py
+++ Scheduler/scheduler.py
@@ -1,1 +1,4 @@
 from Scheduler.Logic import logic
+from itertools import combinations
+
+
