49cafe9c196aa40502b61b961fbd5b2a1bfae3c0
Sihan <alvinalan01@outlook.com>
2023-03-25 14:37:54 +0800

    changes

---
+++ Scheduler/Logic/elevator_manager.py
@@ -0,0 +1,15 @@
+import Scheduler.Thread.thread1 as thread
+import Scheduler.Logic.parser as parser
+
+
+# TODO This needs total rework to first build generate elevator objects then initiating dispatcher
+
+class ElevatorManager:
+    def __init__(self):
+        pass
+
+    def process_raw_info(self):
+        pass
+
+    def kickstart(self):
+        pass
